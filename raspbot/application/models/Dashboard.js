78bbd70b0a812ded164eb7efe28113f900738d37
dpienczykowski <dpienczykowski@gmail.com>
2020-03-19 00:55:57 +0100

    - build

--- raspbot/application/models/Dashboard.js
+++ raspbot/application/models/Dashboard.js
@@ -18,10 +18,11 @@ exports.getTempInformation = () => {
 	return new Promise((resolve, reject) => {
 		const pyshell = require('python-shell');
 		const options = { mode: 'json', args: [], scriptPath: '../../boot/programs' };
-		pyshell.run('pokaz_temp.py', options, (error, response) => {
+		pyshell.run('../../boot/programs/pokaz_temp.py', null, (error, response) => {
 			if (error) {
 				reject(error);
 			} else {
+				console.log('response', response);
 				resolve(response[0]);
 			}
 		});
